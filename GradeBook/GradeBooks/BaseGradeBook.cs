106ce72c3e2d1e145d0d7e6517eacb3306ccdbbf
Tomasz Smykowski <tomasz@smykow.ski>
2020-03-28 01:18:54 +0100

    7.1-6

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,13 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -107,18 +109,19 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int modifier = (IsWeighted && (studentType == StudentType.DualEnrolled || studentType == StudentType.Honors)) ? 1 : 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + modifier;
                 case 'B':
-                    return 3;
+                    return 3 + modifier;
                 case 'C':
-                    return 2;
+                    return 2 + modifier;
                 case 'D':
-                    return 1;
+                    return 1 + modifier;
                 case 'F':
-                    return 0;
+                    return 0 + modifier;
             }
             return 0;
         }
